dd4e9d8c6ce47ce73311fe25aa1d8d891b22ecfd
NovakAnton <antonijevic1999@gmail.com>
2024-03-27 19:16:45 +0100

    Updated the sound to play on keydiwb, instead of keyup.

--- index.js
+++ index.js
@@ -35,24 +35,31 @@ document.querySelector(".l-kick").addEventListener("click", function () {
 
 document.addEventListener("keydown", function (event) {
   if ((event.key === "w") | (event.key === "W")) {
+    button.click();
     let button = document.querySelector(".w-tom1");
     button.classList.add("active");
   } else if ((event.key === "a") | (event.key === "A")) {
+    button.click();
     let button = document.querySelector(".a-tom2");
     button.classList.add("active");
   } else if ((event.key === "s") | (event.key === "S")) {
+    button.click();
     let button = document.querySelector(".s-tom3");
     button.classList.add("active");
   } else if ((event.key === "d") | (event.key === "D")) {
+    button.click();
     let button = document.querySelector(".d-tom4");
     button.classList.add("active");
   } else if ((event.key === "j") | (event.key === "J")) {
+    button.click();
     let button = document.querySelector(".j-snare");
     button.classList.add("active");
   } else if ((event.key === "k") | (event.key === "K")) {
+    button.click();
     let button = document.querySelector(".k-crash");
     button.classList.add("active");
   } else if ((event.key === "l") | (event.key === "L")) {
+    button.click();
     let button = document.querySelector(".l-kick");
     button.classList.add("active");
   }
@@ -61,31 +68,24 @@ document.addEventListener("keydown", function (event) {
 document.addEventListener("keyup", function (event) {
   if ((event.key === "w") | (event.key === "W")) {
     let button = document.querySelector(".w-tom1");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "a") | (event.key === "A")) {
     let button = document.querySelector(".a-tom2");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "s") | (event.key === "S")) {
     let button = document.querySelector(".s-tom3");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "d") | (event.key === "D")) {
     let button = document.querySelector(".d-tom4");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "j") | (event.key === "J")) {
     let button = document.querySelector(".j-snare");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "k") | (event.key === "K")) {
     let button = document.querySelector(".k-crash");
-    button.click();
     button.classList.remove("active");
   } else if ((event.key === "l") | (event.key === "L")) {
     let button = document.querySelector(".l-kick");
-    button.click();
     button.classList.remove("active");
   }
 });
